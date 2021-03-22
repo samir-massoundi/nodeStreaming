d2b20e11c74745759ba21efc1ef968c99038a9dd
tonylch <tony.lachaise@ynov.com>
2021-03-22 14:00:10 +0100

    init express

---
+++ server.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = 3000 || process.env.PORT;
+
+app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT} \nApp running at:\n- Local: http://localhost:${PORT}/`));
